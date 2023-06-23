4905b4ea0e09431c1e77b22a57c6dd766c3a28cf
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2023-06-23 15:06:02 +0900

    자동완성 컴포넌트 개발 (2)

--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -1,39 +1,68 @@
 import styled from "styled-components"
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import { dataArr } from "../data/datalist"
 
 export default function AutoComplete(){
     const [text,setText]=useState("")
     const [result,setResult]=useState([])
+    const inputRef = useRef(null);
     const onChange=(event)=>{
         const {target:{value}}=event
         setText(value)
     }
+    const onClick=(event)=>{
+        console.log('라우팅')
+        event.stopPropagation()
+        const {target:{innerText}}=event
+        setText(innerText)
+        setResult([])
+        //return 이후 라우팅처리
+    }
+    const keyDown = (event) => { //화살표 방향키 입력시 검색어 바뀌는 기능 좀 더 생각해 봐야 할 듯
+        // if (event.key === 'ArrowDown'){
+        //     let idx=0
+        //     AutoInput.innerText='123'
+        //     idx++
+        // }
+        // else if (event.key==='ArrowUp'){
 
+        // }
+      };
     useEffect(()=>{
-        if(!text) return
+        if(!text) return setResult([])
         const res=dataArr.filter(value=>value.name.includes(text))
         setResult(res)
     },[text])
-    console.log(text)
+
+    // const isInputFocused = () => {
+    //     if (inputRef.current === document.activeElement) {
+    //     } else { //unfocus
+    //       setText("")
+    //     }
+    //   };
+    
     return (
         <Wrapper>
-            <AutoInput type="text" value={text} onChange={onChange}/>
-            {result.length?
+            <AutoInputWrapper onBlur={()=>console.log('포커스 아웃')}>
+                <AutoInput ref={inputRef} type="text" value={text} onChange={onChange} placeholder="검색어를 입력해 주세요" onKeyDown={keyDown}/>
+                {result.length?
                 <AutoResultWrapper>
-                {result.map((v)=>(
-                    <AutoResult>{v.name}</AutoResult>
+                {result.map((v,idx)=>(
+                    <AutoResult key={idx} onClick={onClick}>{v.name}</AutoResult> //검색결과 나오는 부분
                 ))}
                 </AutoResultWrapper>:
-                '업서요'}
+                null}
+            </AutoInputWrapper>
         </Wrapper>
     )
 }
 
 const Wrapper=styled.div`
-
+    border:1px solid black;
 `
-
+const AutoInputWrapper=styled.div`
+    
+`
 const AutoInput=styled.input`
     width:500px;
     height:20px;
@@ -42,6 +71,7 @@ const AutoInput=styled.input`
 const AutoResultWrapper=styled.div`
     width:500px;
     border:2px solid black;
+    border-top:none;
 `
 
 const AutoResult=styled.div`
